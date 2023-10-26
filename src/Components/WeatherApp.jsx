5b2a9c7ad70071c6c33693cf0a6e812f4cb56cc0
alibinejazz <ali.ejaz@xloopdigital.com>
2023-10-26 18:37:07 +0500

    a little css to be done

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -10,10 +10,8 @@ const WeatherApp = () => {
   const [searching, setSearching] = useState("Karachi");
 
   const fetchUserData = () => {
-    const api_key = "d07f9e786c5b0c3984d57a8ec5614d94";
-
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${searching}&appid=${api_key}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${searching}&appid=d07f9e786c5b0c3984d57a8ec5614d94`
     )
       .then((response) => {
         if (!response.ok) {
@@ -31,9 +29,17 @@ const WeatherApp = () => {
 
   useEffect(() => {
     fetchUserData();
-  }, []);
+  }, [searching]);
 
-  let tempincelsius = (city.main.temp - 273).toFixed(2);
+  let tempincelsius = (city.main?.temp - 273.15).toFixed(2);
+
+  const currenttime = new Date();
+  const time = currenttime.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "UTC",
+    hour12: false,
+  });
 
   return (
     <div className="main">
@@ -50,6 +56,7 @@ const WeatherApp = () => {
           >
             <input
               type="search"
+              placeholder="Enter city or country name"
               onChange={(e) => setSearching(e.target.value)}
               className="inputfield"
               style={{ borderRadius: "10px" }}
@@ -57,53 +64,70 @@ const WeatherApp = () => {
             <img src={search} alt="search" width="30px" />
           </div>
           <div>
-            <h1>{searching}</h1>
+            <h1 style={{fontWeight:"1000"}}>{city?.name}</h1>
             <h1>{tempincelsius}° </h1>
+            <h1>{time} GMT</h1>
           </div>
-          {city.sys.country}
+          {city.sys?.country}
           <div
             style={{
               display: "flex",
               flexWrap: "wrap",
-              flexDirection: "column",
+              flexDirection:"column",
+              alignItems:"center",
+              gap:"10px"
             }}
           >
             <div
               style={{
                 display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "center",
+                justifyContent:"center",
                 alignItems: "center",
-                gap: "20px",
+                flexWrap:"wrap",
               }}
             >
-              Wind Speed {city.wind.speed} m/s
-              <img src={wind} alt="wind" width="80px" />
+              Wind Speed {city.wind?.speed} m/s
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/011/739/171/original/3d-rendering-wind-illustration-isolated-png.png"
+                alt="wind"
+                width="80px"
+              />
             </div>
             <div
               style={{
                 display: "flex",
                 flexWrap: "wrap",
-                justifyContent: "center",
+                justifyContent:"center",
                 alignItems: "center",
-                gap: "20px",
               }}
             >
-              rain <img src={rain} alt="wind" width="80px" />
+              Clouds {city.clouds?.all} %{" "}
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/019/198/212/original/3d-cloud-white-3d-weather-element-png.png"
+                alt="wind"
+                width="100px"
+              />
             </div>
             <div
               style={{
                 display: "flex",
                 flexWrap: "wrap",
-                justifyContent: "center",
+                justifyContent:"center",
                 alignItems: "center",
-                gap: "20px",
               }}
             >
-              dry
-              <img src={dry} alt="rain" width="80px" />
+              Humidity {city.main?.humidity} %
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/024/984/177/original/3d-weather-forecast-icon-raindrops-air-humidity-percentage-3d-illustration-png.png"
+                alt="rain"
+                width="130px"
+              />
             </div>
+            
           </div>
+          {city && city.weather && city.weather.length > 0 && (
+              <h2>{city.weather[0].description} today !</h2>
+            )}
         </div>
       </div>
     </div>
